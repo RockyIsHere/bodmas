6ab6d011de1df05b5637284c0620bc87fc8f8d80
lohith <lohithhggjc@gmail.com>
2022-01-26 16:10:03 +0530

    Basic UI is Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bodmas_comp/screens/game_screen.dart';
+import 'package:bodmas_comp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: GameScreen(),
+      home: const HomeScreen(),
     );
   }
 }
